5332f2492b8e252d288150c9983ba1d089e51197
Guilherme Goncalves <guilhermegufes@gmail.com>
2020-11-04 17:43:54 -0300

    aula1

---
+++ microservices/b/b.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type Result struct {
+	Status string
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":9091", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	coupon := r.PostFormValue("coupon")
+	ccNumber := r.PostFormValue("ccNumber")
+
+	resultCoupon := makeHttpCall("http://localhost:9092", coupon)
+
+	result := Result{Status: "declined"}
+
+	log.Println(r.PostFormValue("ccNumber"))
+	if ccNumber == "1" {
+		result.Status = "approved"
+	}
+
+	if resultCoupon.Status == "invalid" {
+		result.Status = "invalid coupon"
+	}
+
+	jsonData, err := json.Marshal(result)
+	if err != nil {
+		log.Fatal("Error processing json")
+	}
+
+	fmt.Fprint(w, string(jsonData))
+}
+
+func process(w http.ResponseWriter, r *http.Request) {
+
+	result := makeHttpCall("hhtp://localhost:9091", r.FormValue("coupon"))
+	t := template.Must(template.ParseFiles("templates/home.html"))
+	t.Execute(w, result)
+}
+
+func makeHttpCall(urlMicroservice string, coupon string) Result {
+
+	values := url.Values{}
+	values.Add("coupon", coupon)
+
+	res, err := http.PostForm(urlMicroservice, values)
+
+	if err != nil {
+		result := Result{Status: "Server cannot be found"}
+		return result
+	}
+
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Fatal("Error processing result")
+	}
+
+	result := Result{}
+
+	json.Unmarshal(data, &result)
+
+	return result
+}
---
+++ microservices/go.mod
@@ -0,0 +1,5 @@
+module github.com/guilhermegoncalvess
+
+go 1.15
+
+require github.com/hashicorp/go-retryablehttp v0.6.7
